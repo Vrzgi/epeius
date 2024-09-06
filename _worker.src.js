54ad03c3f187e542385f97b6bea70d2e26c7f8f9
Hamid Reza Vorezgi Nejad <33749060+Vrzgi@users.noreply.github.com>
2024-09-06 16:07:31 +0330

    Update _worker.src.js

--- _worker.src.js
+++ _worker.src.js
@@ -1,6 +1,6 @@
 // _worker.src.js
 import { connect } from "cloudflare:sockets";
-let password = 'auto';
+let password = 'adminPanel';
 let proxyIP = '';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
